fa0fb6404bbf93059189b662594032ed48fb8a15
Vivek Pathak <coolvivekpathak2004@gmail.com>
2024-01-08 02:04:12 +0530

    offline functionality

--- Home.jsx
+++ Home.jsx
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useEffect, useState } from "react";
 import {
   View,
@@ -15,26 +16,60 @@ const Home = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedImage, setSelectedImage] = useState({});
 
+  //   useEffect(() => {
+  //     const fetchData = async () => {
+  //       const response = await fetch(
+  //         "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&per_page=20&page=1&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s"
+  //       );
+  //       const responseData = await response.json();
+  //       const photos = responseData.photos.photo;
+  //       const photoObj = photos.map((photo, index) => {
+  //         const image = photo.url_s;
+  //         const height = photo.height_s;
+  //         const width = photo.width_s;
+  //         const imgObj = {
+  //           id: index,
+  //           imageUrl: image,
+  //           height: height,
+  //           width: width,
+  //         };
+  //         return imgObj;
+  //       });
+  //       setList(photoObj);
+  //     };
+  //     fetchData();
+  //   }, []);
+
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(
-        "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&per_page=20&page=1&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s"
-      );
-      const responseData = await response.json();
-      const photos = responseData.photos.photo;
-      const photoObj = photos.map((photo, index) => {
-        const image = photo.url_s;
-        const height = photo.height_s;
-        const width = photo.width_s;
-        const imgObj = {
-          id: index,
-          imageUrl: image,
-          height: height,
-          width: width,
-        };
-        return imgObj;
-      });
-      setList(photoObj);
+      try {
+        const savedData = await AsyncStorage.getItem("photos");
+        if (savedData !== null) {
+          setList(JSON.parse(savedData));
+        } else {
+          const response = await fetch(
+            "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&per_page=20&page=1&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s"
+          );
+          const responseData = await response.json();
+          const photos = responseData.photos.photo;
+          const photoObj = photos.map((photo, index) => {
+            const image = photo.url_s;
+            const height = photo.height_s;
+            const width = photo.width_s;
+            const imgObj = {
+              id: index,
+              imageUrl: image,
+              height: height,
+              width: width,
+            };
+            return imgObj;
+          });
+          setList(photoObj);
+          await AsyncStorage.setItem("photos", JSON.stringify(photoObj));
+        }
+      } catch (error) {
+        console.error(error);
+      }
     };
     fetchData();
   }, []);
