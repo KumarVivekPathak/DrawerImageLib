c2ce527a5d2afb3160701c9c8d8df59e15efe9cb
Vivek Pathak <coolvivekpathak2004@gmail.com>
2024-01-08 02:19:40 +0530

    offline functionality with net

--- Home.jsx
+++ Home.jsx
@@ -17,30 +17,6 @@ const Home = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedImage, setSelectedImage] = useState({});
 
-  //   useEffect(() => {
-  //     const fetchData = async () => {
-  //       const response = await fetch(
-  //         "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&per_page=20&page=1&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s"
-  //       );
-  //       const responseData = await response.json();
-  //       const photos = responseData.photos.photo;
-  //       const photoObj = photos.map((photo, index) => {
-  //         const image = photo.url_s;
-  //         const height = photo.height_s;
-  //         const width = photo.width_s;
-  //         const imgObj = {
-  //           id: index,
-  //           imageUrl: image,
-  //           height: height,
-  //           width: width,
-  //         };
-  //         return imgObj;
-  //       });
-  //       setList(photoObj);
-  //     };
-  //     fetchData();
-  //   }, []);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
